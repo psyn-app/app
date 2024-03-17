b11654ee17f2827fba09ea04525990509befc409
VisionMise <visionmise@psyn.app>
2024-03-17 11:06:07 -0400

    Initial Commit

---
+++ app.ts
@@ -0,0 +1,12 @@
+/**
+ * psyn.app
+ * 
+ * A simple, fast, and secure web application framework.
+ * 
+ * jsr.io/@psyn/app
+ * github.com/psyn-app/app
+ * 
+ * @author VisionMise <visionmise@psyn.app>
+ * @license MIT
+ * @version 0.1.0
+ */
